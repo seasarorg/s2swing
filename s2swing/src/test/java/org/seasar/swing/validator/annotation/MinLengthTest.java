cd57d2fc336b9249c7bfdc01cd62eb787d8fbaf9
kaiseh <kaisei.hamamoto@gmail.com>
2008-02-08 13:02:23 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2swing/trunk@25 87dd747e-107f-4d8e-b9fb-3c904564fcbc

---
+++ s2swing/src/test/java/org/seasar/swing/validator/annotation/MinLengthTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.seasar.swing.validator.annotation;
+
+import org.seasar.swing.validator.S2Validator;
+
+import junit.framework.TestCase;
+
+/**
+ * @author kaiseh
+ */
+
+public class MinLengthTest extends TestCase {
+    public static class Aaa {
+        @MinLength(4)
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public void test() {
+        Aaa aaa = new Aaa();
+
+        aaa.setName("123");
+        assertFalse(S2Validator.isValid(aaa));
+
+        aaa.setName("1234");
+        assertTrue(S2Validator.isValid(aaa));
+    }
+}
